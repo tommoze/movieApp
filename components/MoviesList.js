6d8d1471b1002eb1543250f031502f2743c95f10
tommoze <33844718+tommoze@users.noreply.github.com>
2022-01-10 21:06:05 +0200

    memoize MoviesList

--- components/MoviesList.js
+++ components/MoviesList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import {
     StyleSheet,
     Text,
@@ -13,34 +13,54 @@ import MovieCard from './MovieCard'
 import ErrorMesage from './ErrorMesage'
 import { lightColor } from '../helpers/colors'
 
+const keyExtractor = (item) => item.id
+const renderItem =
+    (onPress) =>
+    ({ item }) =>
+        (
+            <MovieCard
+                title={item.title}
+                imgUrl={item.poster_path}
+                onPress={onPress(item)}
+            />
+        )
+
 const MoviesList = ({ navigation, category, categoryTitle, id }) => {
     const [isLoading, setLoading] = useState(true)
     const [movies, setMovies] = useState([])
     const [error, setError] = useState(false)
     const [page, setPage] = useState(1)
 
-    const getMovies = async () => {
+    const getMovies = useCallback(async () => {
         try {
             const response = await axios.get(`${URL}${category}${page}`)
             const results = response.data.results
 
-            if (movies) {
-                setMovies([...movies, ...results])
-            } else {
-                setMovies(results)
-            }
+            setMovies([...movies, ...results])
             setError(false)
-        } catch (error) {
-            console.error(error)
+        } catch (err) {
+            console.error(err)
             setError(true)
         } finally {
             setLoading(false)
         }
-    }
+    }, [setMovies, setError, setLoading, category, page, movies])
 
-    useEffect(() => {
-        getMovies()
-    }, [page])
+    const onEndReached = useCallback(() => setPage(page + 1), [setPage, page])
+
+    const onPress = useCallback(
+        (item) => () =>
+            navigation.push('Details', {
+                title: item.title,
+                imgUrl: item.poster_path,
+                overview: item.overview,
+                id: item.id,
+                date: item.release_date,
+            }),
+        [navigation]
+    )
+
+    useEffect(getMovies, [page])
 
     return (
         <View style={{ flex: 1 }}>
@@ -48,32 +68,14 @@ const MoviesList = ({ navigation, category, categoryTitle, id }) => {
             {isLoading ? (
                 <ActivityIndicator size="large" color="#0000ff" />
             ) : error ? (
-                <ErrorMesage text={'Ops, something went wrong!'} />
+                <ErrorMesage text="Ops, something went wrong!" />
             ) : (
                 <FlatList
                     horizontal={true}
                     data={movies}
-                    keyExtractor={(item, index) => index.toString()}
-                    onEndReached={() => {
-                        setPage(page + 1)
-                    }}
-                    renderItem={({ item }) => (
-                        <View>
-                            <MovieCard
-                                title={item.title}
-                                imgUrl={item.poster_path}
-                                onPress={() =>
-                                    navigation.push('Details', {
-                                        title: item.title,
-                                        imgUrl: item.poster_path,
-                                        overview: item.overview,
-                                        id: item.id,
-                                        date: item.release_date,
-                                    })
-                                }
-                            />
-                        </View>
-                    )}
+                    keyExtractor={keyExtractor}
+                    onEndReached={onEndReached}
+                    renderItem={renderItem(onPress)}
                 />
             )}
         </View>
